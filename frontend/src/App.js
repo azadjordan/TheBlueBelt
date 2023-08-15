d2875f4ba3d72ac4a476d92c6e33cf9c5c4c46f3
Azad Khaled Alkurdi <azadkkurdi@gmail.com>
2023-08-15 19:00:20 +0400

    fetching products from local server not from a db yet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
+
 
 const App = () => {
   return (
-    <h1>Welcome to Azad shop</h1>
-  )
-}
+    <BrowserRouter>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer/>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
