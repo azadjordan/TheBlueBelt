3ed2e795262acfee2c302a2470b2db44abc60d71
Azad Khaled Alkurdi <azadkkurdi@gmail.com>
2023-11-24 16:01:34 +0400

    jwt secure set to false

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,7 +6,7 @@ const generateToken = (res, userId)=>{
         // Set JWT as JTTP-Only cookie
         res.cookie('jwt', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV !== 'development',
+            secure: false,
             sameSite: 'strict',
             maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
         })
