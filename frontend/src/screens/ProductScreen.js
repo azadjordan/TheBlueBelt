950c08764a2a2c5010dbbd9e697f0b461826b514
Azad Khaled Alkurdi <azadkkurdi@gmail.com>
2023-12-12 19:25:28 +0400

    edit productScreen columns

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -45,10 +45,8 @@ const ProductScreen = () => {
 
   return (
     <>
-      <Link className="btn btn-light my-3" to="/">
-        Go Back
-      </Link>
-
+      <Link className="btn btn-light my-3" to="/">Go Back</Link>
+  
       {productStatus === 'loading' ? (<Loader />) :
         productStatus === 'failed' ? (
           <Message variant='danger'>
@@ -57,15 +55,13 @@ const ProductScreen = () => {
         ) : productStatus === 'succeeded' && (
           <>
             <Meta title={product.name} />
-
+  
             <Row>
-              <Col md={5}>
-                {/* Main Image */}
+              <Col md={6}>
+                {/* Main Image and Thumbnails */}
                 <div className="product-image-container">
                   <Image className="d-block zoom-effect" src={product.images[activeImage]} alt={product.name} fluid />
                 </div>
-
-                {/* Thumbnails */}
                 <Row className="mt-3">
                   {product.images.map((imgUrl, index) => (
                     <Col xs={4} md={3} key={index}>
@@ -80,25 +76,9 @@ const ProductScreen = () => {
                     </Col>
                   ))}
                 </Row>
-              </Col>
-              <Col md={4}>
-                <ListGroup variant="flush">
-                  <ListGroup.Item>
-                    <h2>{product.name}</h2>
-                  </ListGroup.Item>
-                  {/* <ListGroup.Item>
-                    <Rating
-                      value={product.rating}
-                      text={`${product.numReviews} reviews`}
-                    />
-                  </ListGroup.Item> */}
-                  <ListGroup.Item> Size: <strong>{product.dimensions}</strong> </ListGroup.Item>
-                  <ListGroup.Item>Price: AED {product.price}</ListGroup.Item>
-                  <ListGroup.Item>Description: {product.description}</ListGroup.Item>
-                </ListGroup>
-              </Col>
-              <Col md={3}>
-                <Card>
+  
+                {/* Add to Cart Section */}
+                <Card className="mt-3">
                   <ListGroup variant="flush">
                     <ListGroup.Item>
                       <Row>
@@ -110,7 +90,7 @@ const ProductScreen = () => {
                     </ListGroup.Item>
                     <ListGroup.Item>
                       <Row>
-                        <Col>productStatus:</Col>
+                        <Col>Product Status:</Col>
                         <Col>
                           <strong>
                             {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
@@ -118,7 +98,6 @@ const ProductScreen = () => {
                         </Col>
                       </Row>
                     </ListGroup.Item>
-
                     {product.countInStock > 0 && (
                       <ListGroup.Item>
                         <Row>
@@ -138,7 +117,6 @@ const ProductScreen = () => {
                         </Row>
                       </ListGroup.Item>
                     )}
-
                     <ListGroup.Item>
                       <Button
                         className="btn-block"
@@ -152,10 +130,21 @@ const ProductScreen = () => {
                   </ListGroup>
                 </Card>
               </Col>
+  
+              <Col md={6}>
+                {/* Product Details */}
+                <ListGroup variant="flush">
+                  <ListGroup.Item>
+                    <h2>{product.name}</h2>
+                  </ListGroup.Item>
+                  <ListGroup.Item> Size: <strong>{product.dimensions}</strong> </ListGroup.Item>
+                  <ListGroup.Item>Price: AED {product.price}</ListGroup.Item>
+                  <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+                </ListGroup>
+              </Col>
             </Row>
           </>
         )}
-
     </>
   );
 };
