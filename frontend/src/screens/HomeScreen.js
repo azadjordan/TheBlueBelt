1527f6da4aad1c176b09cfcc92dbb1e42a95d71c
Azad Khaled Alkurdi <azadkkurdi@gmail.com>
2024-01-20 19:43:08 +0400

    change height

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -129,7 +129,7 @@ const HomeScreen = () => {
         <>
           <Row>
             {data.products.map((product) => (
-              <Col key={product._id} xl={2} lg={3} md={4} sm={6} xs={6}  >
+              <Col key={product._id} xl={3} lg={3} md={4} sm={6} xs={6}  >
                 <Product product={product} />
               </Col>
             ))}
