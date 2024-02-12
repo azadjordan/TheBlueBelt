8c4f1e4291b9509958fcc9761ebab25b8f1915fc
Azad Khaled Alkurdi <azadkkurdi@gmail.com>
2024-02-12 23:11:03 +0400

    edit ribbon card 2

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -10,18 +10,18 @@ const Product = ({ product }) => {
 
       <Card.Body className="card-body-home-screen">
         <Link to={`/product/${product._id}`} className="text-decoration-none">
-          <Card.Text className="text-truncate" style={{ fontSize: '1.2rem' }}>
+          <Card.Text className="p-0 m-0 text-truncate" style={{ fontSize: '1rem' }}>
             {product.name}
           </Card.Text>
         </Link>
 
-        <Card.Text className="mb-0"><strong>{product.dimensions}</strong></Card.Text>
+        <Card.Text className="product-dimensions-tag mb-0"><strong>{product.dimensions}</strong></Card.Text>
 
         {/* Conditionally render the stock status with color coding */}
         <Card.Text className="pb-0 mb-0 price-stock-container" style={product.countInStock === 0 ? { color: 'red' } : { color: 'green' }}>
 
-          <span className="product-price-tag"> AED {product.price} </span>
-          <span className="in-stock-tag">{product.countInStock === 0 ? "No Stock" : "In Stock"}</span> 
+          <span className="product-price-tag">AED{product.price}</span>
+          <span className="in-stock-tag">{product.countInStock === 0 ? "No-Stock" : "In-Stock"}</span> 
           {/* <h3>{product.countInStock}</h3>  */}
 
         </Card.Text>
